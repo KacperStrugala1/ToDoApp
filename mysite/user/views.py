b30b9169401f5a1c5fdbd851733f3b9ae0a63084
KacperStrugala1 <kacperstrugala5@gmail.com>
2025-08-05 20:24:09 +0200

    New messages

--- mysite/user/views.py
+++ mysite/user/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from user.forms import UserForm
 
-# Create your views here.
+def register(request):
+    if request.method == "POST":
+        user_form = UserForm(request.POST)
+        if user_form.is_valid():
+            user = user_form.save(commit=False)  # NIE zapisujemy od razu
+            raw_password = user_form.cleaned_data['password']
+            user.set_password(raw_password)  # Teraz hasło będzie haszowane
+            user.save()
+            return redirect("/")
+        else:
+            print(user_form.errors)
+    else:
+        user_form = UserForm()
+
+    return render(request, "user/register.html", {"user_form": user_form})
